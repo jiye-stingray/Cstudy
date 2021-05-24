638867250bfb15960908bc48d72cf23ea1a08aa0
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2021-05-24 13:51:32 +0900

    Add files via upload

---
+++ 문자열 복사.c
@@ -0,0 +1,42 @@
+//#include <stdio.h>
+//#pragma warning (disable: 4996)
+//int main() {
+//	
+	//문자열 복사하기
+	/*char scr[] = "The worst things to eat before you sleep";
+	char dst[100];
+	int i;
+	printf("원본 문자열 = %s\n", scr);
+	for ( i = 0; scr[i] != '\0'; i++)
+	{
+		dst[i] = scr[i];
+	}
+	dst[i] = '\0';
+	printf("복사된 문자열 = %s\n", dst);
+	printf("문자 갯수: %d", i);*/
+
+	
+	/*char s[3][6] = {
+		"init",
+		"open",
+		"close"
+	};
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; s[i][j] != '\0'; j++)
+		{
+			printf("%c", s[i][j]);
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i<3; i++)
+	{	
+		printf("%s\n", s[i]);
+	}*/
+	
+
+	
+//	return 0;
+//}
